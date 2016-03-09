d4b605e7f296bc98743524e54ede6f8a23462287
C-A de Salaberry <cadesalaberry@yahoo.com>
2016-03-09 23:08:52 +0100

    Add: Clicks between menus.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,54 @@ import React, { Component } from 'react';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {opened: false};
+    this.state = {
+      opened: false,
+      tab: 'home',
+    };
   }
 
-  click() {
+  clickHome() {
     console.log('Clicked');
-    this.setState({opened: true});
+    this.setState({ opened: true });
+  }
+
+  clickVilla() {
+    console.log('Clicked villa');
+    this.setState({ tab: 'villa' });
+  }
+
+  clickModel() {
+    console.log('Clicked villa');
+    this.setState({ tab: 'model' });
   }
 
   render() {
     console.log(this.state.opened);
+    var home = 'home';
     var left = 'left';
     var right = 'right'
     var leftTitle = 'title left-title';
     var rightTitle = 'title right-title';
 
-    left += this.state.opened ? ' left--opened' : '';
-    right += this.state.opened ? ' right--opened' : '';
+    home += this.state.tab !== 'home'
+    ? ' home--' + this.state.tab
+    : '';
+    
+    left += this.state.opened
+    ? ' left--opened'
+    : '';
+
+    right += this.state.opened
+    ? ' right--opened'
+    : '';
 
     return (
-      <h1 className="home" onClick={this.click.bind(this)}>
+      <h1 className={ home } onClick={this.clickHome.bind(this)}>
         <div className={ left }>
           <div className="title">
             <div className="text">De l’Inspiration</div>
           </div>
-          <div className="subtitle">
+          <div className="subtitle" onClick={this.clickVilla.bind(this)}>
             <div className="text">LA VILLA WEISSENHOF</div>
           </div>
         </div>
@@ -35,7 +58,7 @@ export default class App extends Component {
           <div className="title">
             <div className="text">...À la Création</div>
           </div>
-          <div className="subtitle">
+          <div className="subtitle" onClick={this.clickModel.bind(this)}>
             <div className="text">LA MAQUETTE DE ROMANE</div>
           </div>
         </div>
